b9a844940fd3ba72f3cc0a47a2c443e2039596a1
b.andrusenko <b.andrusenko@andersenlab.com>
2024-06-20 15:47:40 +0300

    init

---
+++ src/main/java/com/ghost/keycloakexperiments/KeycloakExperimentsApplication.java
@@ -0,0 +1,13 @@
+package com.ghost.keycloakexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakExperimentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakExperimentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ghost/keycloakexperiments/KeycloakExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghost.keycloakexperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakExperimentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
