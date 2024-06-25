717135435b66b93522027fd7cb75fa76de8e8d44
Yevhen Valkovskyi <y.valkovskyi@kulu.com>
2024-06-25 10:43:04 +0300

    Add keycloak roles

--- src/main/java/com/ghost/keycloakexperiments/controller/MainController.java
+++ src/main/java/com/ghost/keycloakexperiments/controller/MainController.java
@@ -1,5 +1,8 @@
 package com.ghost.keycloakexperiments.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,12 @@ public class MainController {
     @GetMapping("/home")
     public OAuth2AuthorizedClient home(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
         return authorizedClient;
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("admin/home")
+    public Authentication adminHome() {
+        return SecurityContextHolder.getContext().getAuthentication();
     }
 
 }
